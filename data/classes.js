00d0ec06c580e4757897e7df5bf7c9a099a21c64
ghm1979 <galit.menahem79@gmail.com>
2023-12-07 22:27:30 +0200

    Create register and Login

---
+++ data/classes.js
@@ -0,0 +1,10 @@
+export default class User {
+    constructor(id,fname, lname, age, userName, password) {
+        this.id=id;
+        this.fname = fname;
+        this.lname = lname;       
+        this.age = age;
+        this.userName = userName;
+        this.password = password;
+    }
+}
