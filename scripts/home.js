2fa167dd050aae5dfbaa2babf8750fce57c170ad
אלמג מהנדסי קרינה <moreladu@gmaile.com>
2023-12-10 10:48:48 +0200

    home page almost ready

--- scripts/home.js
+++ scripts/home.js
@@ -1,41 +1,106 @@
+localStorage.setItem("currentUserData", "1")
+// localStorage.removeItem("currentUserData")
+// localStorage.removeItem("current")
+// localStorage.removeItem("photo")
+if (!(localStorage.getItem("currentUserData"))) {
+    window.location.href = "../pages/login.html"
+} else {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../data/professionals.json", true);
+    xhr.onload = function () {
+        localStorage.setItem("Experts", this.responseText);
+    }
+    xhr.send();
+    //NEEDS FIXING redirects to experts.html page 
+    function dispExpert(index) {
+       
+        if (localStorage.getItem("current")) {
+            localStorage.removeItem("current")
+        }
+        localStorage.setItem("current", JSON.stringify(expertsArr[index]));
+        window.location.href = "../pages/experts.html";
+       
+    }
+// shows the cards of the xperts
 
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "../data/professionals.json", true);
-xhr.onload = function () {
-    let expertsDiv = document.getElementById("experts");
-    let response = JSON.parse(this.responseText);
-    response = response.proffesionals;
-    console.log((response));
-    for (let x in response){
-        expertsDiv.innerHTML +=`
-        <img id="" src="">
+    function showExperts() {
+        let expertsDiv = document.getElementById("experts")
+        let expertsArr = JSON.parse(localStorage.getItem("Experts"));
+        expertsArr = expertsArr.proffesionals
+        console.log(expertsArr);
+        for (let x in expertsArr) {
+            expertsDiv.innerHTML += `       
 <ul id="card${x}" class="card">
-    <li>first name: <span>${response[x].fname}</span></li>
-    <li>last name: <span>${response[x].lname}</span></li>
-    <li>proffesion: <span>${response[x].proffesion}</span></li>
-    <li><a id="info${x}"href="">more info...</a></li>
-</ul>`
+<img id="pic${x}">
+    <li>first name: <span>${expertsArr[x].fname}</span></li>
+    <li>last name: <span>${expertsArr[x].lname}</span></li>
+    <li>proffesion: <span>${expertsArr[x].proffesion}</span></li>
+    <button id="btn${x}" onclick="dispExpert(${x})">more info...</button>)
+</ul>`;
+
+            let img = document.getElementById(`pic${x}`)
+            switch (expertsArr[x].proffesion) {
+                case "carpenter":
+                    img.setAttribute("src", "../media/carpenter.png")
+                    img.setAttribute("style", "width:100px;height:100px;")
+                    break;
+                case "lawyer":
+                    img.setAttribute("src", "../media/lawyer.png")
+                    img.setAttribute("style", "width:100px;height:100px;")
+                    break;
+                case "accountant":
+                    img.setAttribute("src", "../media/accountant.png")
+                    img.setAttribute("style", "width:100px;height:100px;")
+                    break;
+                case "plumber":
+                    img.setAttribute("src", "../media/plumber.png")
+                    img.setAttribute("style", "width:100px;height:100px;")
+                    break;
+                case "painter":
+                    img.setAttribute("src", "../media/painter.png")
+                    img.setAttribute("style", "width:100px;height:100px;")
+                    break;
+                case "electrician":
+                    img.setAttribute("src", "../media/electrician.png")
+                    img.setAttribute("style", "width:100px;height:100px;")
+                    break;
+
+                default:
+                    img.setAttribute("src", "../media/data-analyst.png")
+                    img.setAttribute("style", "width:100px;height:100px;")
+                    break;
+            }
+        }
+      
+        
     }
-}
-xhr.send()
-const searchChoice = () =>{
+    showExperts();
+
+
     
-    let choice = document.getElementById("filter");
-    if(choice.value == fname){
-        let lable = document.createElement("label");
-        lable.innerText = "SEARCH FOR EXPERT";
-        lable.setAttribute("id", "lable1");
-        document.getElementById("searchBar").appendChild(lable);
-        let input = document.createElement("input");
-        input.setAttribute("id", "lable1");
-        document.getElementById("searchBar").appendChild(input);
-        let button = document.createElement("button");
-        button.innerText = "SEARCH";
-        button.setAttribute("id", "btn2");
-        document.getElementById("searchBar").appendChild(button);
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
-document.getElementById("btn1").addEventListener("click", searchChoice)
